fb4b46fd5f1b9ce87e4ca43644fcc8114191051f
Iva E. Popova <progressbg.python.course@gmail.com>
2018-08-09 12:45:35 +0300

    clean folders

--- .tmp/comprehensions.py
+++
@@ -1,6 +0,0 @@
-str = "alabala"
-upper_str = [s.capitalize() for s in str]
-print(upper_str)
-
-filtered_upper_str = [s.capitalize() for s in str if s =="a"]
-print(filtered_upper_str)
--- vscode_start.sh
+++
@@ -1,1 +0,0 @@
-.bin/vscode_start.sh
