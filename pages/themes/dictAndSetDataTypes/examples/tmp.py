cdb130dc19f1c82da2f0b5c570d06dec1b5cc844
Iva E. Popova <progressbg.python.course@gmail.com>
2025-02-24 22:51:27 +0200

    update

--- pages/themes/dictAndSetDataTypes/examples/tmp.py
+++ pages/themes/dictAndSetDataTypes/examples/tmp.py
@@ -1,14 +1,2 @@
-### Get max value in list:
-numbers = [2.50, 2.43, 3.50]
-print( max(numbers) )
-
-prices = {
-    "apples": 2.50,
-    "oranges": 2.43,
-    "bananas": 3.50
-}
-### Get max value in a dict
-print( max(prices.values()) )
-
-### Get the key of max value in a dict
-print( max(prices, key=prices.get) )
+skills = {'Python', 'Git'}
+print(f'Skills: {", ".join(skills)}')
