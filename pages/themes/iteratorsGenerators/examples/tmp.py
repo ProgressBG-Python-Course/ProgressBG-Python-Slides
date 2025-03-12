a63b5961c3b2705cadd4c82d750984b4823dc7ec
Iva E. Popova <progressbg.python.course@gmail.com>
2025-03-12 17:01:10 +0200

    update

--- pages/themes/iteratorsGenerators/examples/tmp.py
+++ pages/themes/iteratorsGenerators/examples/tmp.py
@@ -1,2 +1,22 @@
-squares = (num**2 for num in range(1,11) if num%2==0)
-print(list(squares))
+def number_generator(start, end):
+    current = start
+    while current < end:
+        yield current
+        current += 1
+
+
+for num in number_generator(1, 5):
+    print(num, end=",")
+
+
+# def fibonacci_generator(n):
+#     a = 0
+#     b = 1
+
+#     for _ in range(n):
+#         yield a
+#         a, b = b, a + b
+
+
+# for number in fibonacci_generator(10):
+#     print(number, end="")
