05e0dfacc470ade830b1b6801800103246fff4d8
Iva E. Popova <progressbg.python.course@gmail.com>
2025-03-12 17:15:58 +0200

    update

--- pages/themes/iteratorsGenerators/examples/tmp.py
+++ pages/themes/iteratorsGenerators/examples/tmp.py
@@ -1,22 +1,22 @@
-def number_generator(start, end):
-    current = start
-    while current < end:
-        yield current
-        current += 1
+# def number_generator(start, end):
+#     current = start
+#     while current < end:
+#         yield current
+#         current += 1
 
 
-for num in number_generator(1, 5):
-    print(num, end=",")
+# for num in number_generator(1, 5):
+#     print(num, end=",")
 
 
-# def fibonacci_generator(n):
-#     a = 0
-#     b = 1
+def fibonacci_generator(n):
+    a = 0
+    b = 1
 
-#     for _ in range(n):
-#         yield a
-#         a, b = b, a + b
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
 
 
-# for number in fibonacci_generator(10):
-#     print(number, end="")
+for number in fibonacci_generator(10):
+    print(number, end="")
