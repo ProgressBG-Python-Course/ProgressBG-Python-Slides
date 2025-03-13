a64a58183af478b12d774af82e0021e928596ee1
Iva E. Popova <progressbg.python.course@gmail.com>
2025-03-13 14:09:54 +0200

    update

---
+++ pages/themes/Appendix/MoreTasks/find_bug.py
@@ -0,0 +1,42 @@
+# Напишете функция, която намира всички последователности от поне два еднакви
+# елемента в списък и ги показва. Редът на показването няма значение.
+
+# Пример:
+#     вход: numbers=[2, 1, 1, 2, 3, 3, 2, 2, 2, 1],
+#     изход:
+#           [1,1]
+#           [3,3]
+#           [2,2,2]
+
+#     вход: numbers=[4, 4, 2, 2, 2, 3, 3, 1, 4, 4, 4],
+#     изход:
+#          [4, 4]
+#          [2, 2, 2]
+#          [3, 3]
+#          [4, 4, 4]
+
+
+def find_similar(list_of_numbers):
+    similar_lists = []
+
+    for idx in range(1, len(list_of_numbers) - 1):
+        sublist = []
+        if list_of_numbers[idx] == list_of_numbers[idx + 1]:
+            sublist.append(list_of_numbers[idx])
+            sublist.append(list_of_numbers[idx + 1])
+            count = 2
+            while (idx + count) < len(list_of_numbers):
+                if list_of_numbers[idx] == list_of_numbers[idx + count]:
+                    sublist.append(list_of_numbers[idx + count])
+                    count += 1
+                else:
+                    break
+            similar_lists.append(sublist)
+    return similar_lists
+
+
+input_lists = [[2, 1, 1, 2, 3, 3, 2, 2, 2, 1], [4, 4, 2, 2, 2, 3, 3, 1, 4, 4, 4]]
+
+for l in input_lists:
+    print("Output: ")
+    print(find_similar(l))
