97e2762580a2306f7e1b05e2d6e50372ca04adc0
Iva E. Popova <progressbg.python.course@gmail.com>
2025-07-09 17:34:25 +0300

    update

--- pages/themes/dictAndSetDataTypes/Tasks_and_HW/task_simple_word_index.py
+++
@@ -1,9 +0,0 @@
-text = """apple and banana one apple one banana
-          a red apple and a green apple"""
-
-words = text.split()
-
-word_counts = {word:words.count(word) for word in set(words)}
-
-for word,count in word_counts.items():
-    print(f'{word} - {count}')
