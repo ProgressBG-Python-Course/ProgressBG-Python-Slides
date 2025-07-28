26cb461c7c182d02521e5f8828435b9be30b3d76
Iva E. Popova <progressbg.python.course@gmail.com>
2025-07-28 16:52:04 +0300

    update

--- pages/themes/exceptions/examples/simplest_exception.py
+++ pages/themes/exceptions/examples/simplest_exception.py
@@ -1,8 +1,15 @@
-x = 5
-y = 0
-try:
-	res = x/y
-except:
-	print("An exception was raised!")
-else:
-	print("x/y = ", res)
+def get_user_age():
+    while True:
+        try:
+            age = int(input("Enter your age: "))
+            if age < 1:
+                raise ValueError("Age must be between 1 and 99")
+            return age
+        except ValueError:
+            print("Please enter a valid age.")
+
+
+user_age = get_user_age()
+
+# Continue with the program using the valid age
+print(f"You are {user_age} years old.")
