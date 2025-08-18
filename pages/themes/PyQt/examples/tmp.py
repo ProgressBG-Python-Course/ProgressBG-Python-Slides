c0d8535e98a4ecda86e26536b19ebb89da6c3d66
Iva E. Popova <progressbg.python.course@gmail.com>
2025-08-18 17:30:22 +0300

    update

--- pages/themes/PyQt/examples/tmp.py
+++ pages/themes/PyQt/examples/tmp.py
@@ -1,11 +1,11 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication,QWidget
+from PyQt6.QtWidgets import QApplication, QWidget
 
 app = QApplication(sys.argv)
 
 window = QWidget()
-window.setWindowTitle('PyQt6 App Works')
+window.setWindowTitle("PyQt6 App Works")
 window.setGeometry(100, 100, 500, 500)
 
 window.show()
